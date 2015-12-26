2a9b1bfe50cfc30f8789dab4a4eb66ac3c50db17
MartinN13 <martinnabhan@me.com>
2015-12-26 02:24:38 +0100

    Initialize repository

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*jshint multistr: true */
+
+window.Test = (function() {
+    "use strict";
+
+    var Test = {
+        "answers": [
+                "1",
+                "1",
+                "2"
+        ]
+    };
+})();
